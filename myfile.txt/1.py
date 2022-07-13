83746f7927708e65176b65edfd869b937176ae8b
BISGBLY <109197693+BISGBLY@users.noreply.github.com>
2022-07-13 11:12:39 +0530

    Add files via upload

---
+++ myfile.txt/1.py
@@ -0,0 +1,17 @@
+f = open("myfile.txt","w")
+l1 = input("Enter the Text")
+l2 = input("Enter the TEXT 2")
+l3 = input("Enter the text")
+f.write(l1)
+f.write("\n")
+f.write(l2)
+f.write("\n")
+f.write(l3)
+f.write("\n")
+f.close()
+
+print(" Test of myfile.txt :")
+f=open("myfile.txt","r")
+d = f.read()
+print(d)
+f.close
